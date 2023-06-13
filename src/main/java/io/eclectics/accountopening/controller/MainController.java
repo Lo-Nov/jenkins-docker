ca7ca65379c103017284a8d79c86f0aed53e2ea9
Loman Edwin <cheruiyot.edwin@eclectics.io>
2023-06-13 10:42:28 +0300

    WIP on template config

---
+++ src/main/java/io/eclectics/accountopening/controller/MainController.java
@@ -0,0 +1,11 @@
+package io.eclectics.accountopening.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mb-acc-opening/")
+@RequiredArgsConstructor
+public class MainController {
+}
