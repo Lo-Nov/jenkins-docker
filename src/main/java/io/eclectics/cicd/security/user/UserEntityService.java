96eeead5249a817bcf38fde08dd5c4a104c58552
Loman Edwin <cheruiyot.edwin@eclectics.io>
2023-06-13 10:32:58 +0300

    WIP on template config

--- src/main/java/io/eclectics/cicd/security/user/UserEntityService.java
+++
@@ -1,9 +0,0 @@
-package io.eclectics.cicd.security.user;
-
-import reactor.core.publisher.Mono;
-
-public interface UserEntityService {
-    Mono<UserEntity> findByUsername(String username);
-
-    UserEntity saveUser(UserEntity user);
-}
